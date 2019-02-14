35e23a9fa6727a8290416f5baddc5df2865b9e27
Bradford Falmer nightprowler <texetomg@gmail.com>
2019-02-14 19:29:36 +0300

    sraka

--- key_hook.c
+++ key_hook.c
@@ -6,16 +6,24 @@
 /*   By: bfalmer- <bfalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 19:00:12 by bfalmer-          #+#    #+#             */
-/*   Updated: 2019/02/14 19:02:15 by bfalmer-         ###   ########.fr       */
+/*   Updated: 2019/02/14 19:29:24 by bfalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
+
+int     player_move(int key, t_wolf *wolf)
+{
+    if (key )
+    sraka;
+}
 
 int     key_press(int key, t_wolf *wolf)
 {
     (void)wolf;
     if (key == 53)
         exit(1);
+    if (key == 13 || key == 1 || key == 0 || key == 2)
+        player_move(wolf);
     return 0;
 }
