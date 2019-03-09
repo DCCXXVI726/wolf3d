55788675ce2775e63ce6b13692b416657dd0226b
Bradford Falmer nightprowler <texetomg@gmail.com>
2019-03-09 15:35:58 +0300

    norme

--- includes/wolf3d.h
+++ includes/wolf3d.h
@@ -18,62 +18,63 @@
 # include <time.h>
 # include <sys/time.h>
 # define ANGLE 3.14 / 200
-typedef struct  s_player
-{
-	double      x;
-	double      y;
-	double      angle;
-}               t_player;
 
-typedef struct	s_texture
+typedef struct		s_player
 {
-	void		*img_ptr;
-	void		*start_img;
-	int			bpp;
-	int			size_line;
-	int			endian;
-	int			heidth;
-	int			width;
-}				t_texture;
+	double			x;
+	double			y;
+	double			angle;
+}					t_player;
 
-typedef	struct	s_wolf
+typedef struct		s_texture
 {
-	struct timeval			time;
-	struct timeval			old_time;
-	int			win_heidth;
-	int			win_width;
-	void		*mlx_ptr;
-	void		*win_ptr;
-	void		*img_ptr;
-	char		*start_img;
-	int			bpp;
-	int			size_line;
-	int			endian;
-	size_t		limit;
-	t_player	*player;
-	int			width;
-	int			heigth;
-	char		*map;
-	cl_mem		cl_map;
-	int			iteration;
-	double		fov;
-	int			mouse_x;
-	double		step;
-	int			step_count;
-	char		move_forward;
-	char		move_back;
-	char		move_right;
-	char		move_left;
-	int			line_horizon;
-	double		step_ill;
-	t_texture	*tx;
-}				t_wolf;
+	void			*img_ptr;
+	void			*start_img;
+	int				bpp;
+	int				size_line;
+	int				endian;
+	int				heidth;
+	int				width;
+}					t_texture;
 
-int				put_img(t_wolf *wolf);
-t_wolf			*create_struct();
-void    		hook(t_wolf *wolf);
-int     		key_press(int key, t_wolf *wolf);
-int             key_release(int key, t_wolf *wolf);
-void			put_player(t_wolf *wolf);
-int     		mouse_move(int x, int y, t_wolf *wolf);
+typedef	struct		s_wolf
+{
+	struct timeval	time;
+	struct timeval	old_time;
+	int				win_heidth;
+	int				win_width;
+	void			*mlx_ptr;
+	void			*win_ptr;
+	void			*img_ptr;
+	char			*start_img;
+	int				bpp;
+	int				size_line;
+	int				endian;
+	size_t			limit;
+	t_player		*player;
+	int				width;
+	int				heigth;
+	char			*map;
+	cl_mem			cl_map;
+	int				iteration;
+	double			fov;
+	int				mouse_x;
+	double			step;
+	int				step_count;
+	char			move_forward;
+	char			move_back;
+	char			move_right;
+	char			move_left;
+	int				line_horizon;
+	double			step_ill;
+	t_texture		*tx;
+}					t_wolf;
+
+int					put_img(t_wolf *wolf);
+t_wolf				*create_struct();
+void				hook(t_wolf *wolf);
+int					key_press(int key, t_wolf *wolf);
+int					key_release(int key, t_wolf *wolf);
+void				put_player(t_wolf *wolf);
+int					mouse_move(int x, int y, t_wolf *wolf);
 #endif
